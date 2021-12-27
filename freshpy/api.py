6c24a90c06e798382bebf07b21d295db694a61c7
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-27 16:12:13 -0700

    Added functions to freshpy/api.py

--- freshpy/api.py
+++ freshpy/api.py
@@ -13,5 +13,59 @@ import requests
 
 from .utils import log_utils
 
+# Initialize logging
+logger = log_utils.initialize_logging(__name__)
+
+
+def define_headers():
+    """This function defines the headers to use in API calls.
+
+    .. versionadded:: 1.0.0
+    """
+    headers = {'Content-Type': 'application/json'}
+    return headers
+
+
+def define_auth(api_key):
+    """This function defines the authentication dictionary to use in API calls."""
+    credentials = {api_key: 'X'}
+    return credentials
+
+
+def get_request_with_retries(fresh_object, uri, headers=None, return_json=True):
+    """This function performs a GET request and will retry several times if a failure occurs.
+
+    .. versionadded:: 1.0.0
+
+    :param fresh_object: The instantiated :py:class:`freshpy.core.FreshPy` object.
+    :param uri: The URI to query
+    :type uri: string
+    :param headers: The HTTP headers to utilize in the REST API call
+    :type headers: dict, None
+    :param return_json: Determines if JSON data should be returned
+    :type return_json: bool
+    :returns: The JSON data from the response or the raw :py:mod:`requests` response.
+    """
+    # Define headers if not supplied
+    headers = define_headers() if not headers else headers
+
+    # Construct the credentials dictionary
+    credentials = define_auth(fresh_object.api_key)
+
+    retries, response = 0, None
+    while retries <= 5:
+        try:
+            response = requests.get(uri, headers=headers, auth=credentials)
+            break
+        except Exception as exc_msg:
+            # _report_failed_attempt(exc_msg, 'get', retries)
+            retries += 1
+    if retries == 6:
+        # _raise_exception_for_repeated_timeouts()
+        pass
+    if return_json:
+        response = response.json()
+    return response
+
 
 
