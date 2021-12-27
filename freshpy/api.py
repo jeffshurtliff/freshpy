c62f740a8e34eb6924628bb50d43b3c1c4d07a13
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-27 15:42:59 -0700

    Created freshpy/api.py

---
+++ freshpy/api.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            freshpy.api
+:Synopsis:          This module handles interactions with the Freshservice REST API
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     27 Dec 2021
+"""
+
+import json
+
+import requests
+
+from .utils import log_utils
+
+
+
