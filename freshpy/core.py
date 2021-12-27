08db647486736e5860fc5c3dfb6f522a813f0126
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-27 15:31:55 -0700

    Added some attributes to the FreshPy object

--- freshpy/core.py
+++ freshpy/core.py
@@ -16,13 +16,24 @@ logger = log_utils.initialize_logging(__name__)
 class FreshPy(object):
     """This is the class for the core object leveraged in this library."""
     # Define the function that initializes the object instance (i.e. instantiates the object)
-    def __init__(self):
+    def __init__(self, domain=None, api_key=None):
         """This method instantiates the core Fresh object.
 
         .. versionadded:: 1.0.0
         """
         # Define the current version
         self.version = version.get_full_version()
+
+        # Define the domain
+        domain = f'https://{domain}' if domain and not domain.startswith('http') else domain
+        domain = domain[:-1] if domain.endswith('/') else domain
+        self.domain = domain
+
+        # Define the base URL
+        self.base_url = f'{domain}/api/v2/'
+
+        # Define the API key
+        self.api_key = api_key
 
     def __del__(self):
         """This method fully destroys the instance.
