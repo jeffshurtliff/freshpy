32f2c7e3b7bed3c9bcb8314ff8f0c553372f389f
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-27 15:08:56 -0700

    Created the FreshPy object base structure

--- freshpy/core.py
+++ freshpy/core.py
@@ -7,7 +7,32 @@
 :Modified Date:     27 Dec 2021
 """
 
-from .utils import log_utils
+from .utils import log_utils, version
 
 # Initialize logging
 logger = log_utils.initialize_logging(__name__)
+
+
+class FreshPy(object):
+    """This is the class for the core object leveraged in this library."""
+    # Define the function that initializes the object instance (i.e. instantiates the object)
+    def __init__(self):
+        """This method instantiates the core Fresh object.
+
+        .. versionadded:: 1.0.0
+        """
+        # Define the current version
+        self.version = version.get_full_version()
+
+    def __del__(self):
+        """This method fully destroys the instance.
+
+        .. versionadded:: 1.0.0
+        """
+        self.close()
+
+    def close(self):
+        """This core method destroys the instance.
+
+        .. versionadded:: 1.0.0
+        """
