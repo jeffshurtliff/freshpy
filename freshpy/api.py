46047f58744a7a16db781fa2938cd65233e51b38
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-28 09:23:31 -0700

    Updated functionality of api.py

--- freshpy/api.py
+++ freshpy/api.py
@@ -11,6 +11,7 @@ import json
 
 import requests
 
+from . import errors
 from .utils import log_utils
 
 # Initialize logging
@@ -62,14 +63,46 @@ def get_request_with_retries(fresh_object, uri, headers=None, return_json=True):
             response = requests.get(query_url, headers=headers, auth=credentials)
             break
         except Exception as exc_msg:
-            # _report_failed_attempt(exc_msg, 'get', retries)
+            _report_failed_attempt(exc_msg, 'get', retries)
             retries += 1
     if retries == 6:
-        # _raise_exception_for_repeated_timeouts()
+        _raise_exception_for_repeated_timeouts()
         pass
     if return_json:
         response = response.json()
     return response
 
 
+def _report_failed_attempt(_exc_msg, _request_type, _retries):
+    """This function reports a failed API call that will be retried.
 
+    .. versionadded:: 1.0.0
+
+    :param _exc_msg: The exception that was raised can captured within a try/except clause
+    :param _request_type: The type of API request (e.g. ``post``, ``put`` or ``get``)
+    :type _request_type: str
+    :param _retries: The attempt number for the API request
+    :type _retries: int
+    :returns: None
+    """
+    _exc_name = type(_exc_msg).__name__
+    if 'connect' not in _exc_name.lower():
+        raise Exception(f"{_exc_name}: {_exc_msg}")
+    _current_attempt = f"(Attempt {_retries} of 5)"
+    _error_msg = f"The {_request_type.upper()} request has failed with the following exception: " + \
+                 f"{_exc_name}: {_exc_msg} {_current_attempt}"
+    errors.handlers.eprint(f"{_error_msg}\n{_exc_name}: {_exc_msg}\n")
+    return
+
+
+def _raise_exception_for_repeated_timeouts():
+    """This function raises an exception when all API attempts (including) retries resulted in a timeout.
+
+    .. versionadded:: 1.0.0
+
+    :returns: None
+    :raises: :py:exc:`freshpy.errors.exceptions.APIConnectionError`
+    """
+    _failure_msg = "The script was unable to complete successfully after five consecutive API timeouts. " + \
+                   "Please run the script again or contact Freshservice Support for further assistance."
+    raise errors.exceptions.APIConnectionError(_failure_msg)
