814c6d279557ea81db4b34e7a8204c3af8d109ba
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-28 08:53:35 -0700

    Added the freshpy.errors module

---
+++ freshpy/errors/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+:Package:        freshpy.errors
+:Synopsis:       This module includes custom exceptions
+:Created By:     Jeff Shurtliff
+:Last Modified:  Jeff Shurtliff
+:Modified Date:  28 Dec 2021
+"""
