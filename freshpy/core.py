ebdc9a590e3dd384f6412d60e746ab2412633a0a
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-29 15:36:58 -0700

    Added the Tickets class and sub-method

--- freshpy/core.py
+++ freshpy/core.py
@@ -36,6 +36,16 @@ class FreshPy(object):
         # Define the API key
         self.api_key = api_key
 
+        # Import inner object classes so their methods can be called from the primary object
+        self.tickets = self._import_tickets_class()
+
+    def _import_tickets_class(self):
+        """This method allows the :py:class:`freshpy.core.FreshPy.Tickets` class to be utilized in the core object.
+
+        .. versionadded:: 1.0.0
+        """
+        return FreshPy.Tickets(self)
+
     def get(self, uri, headers=None, return_json=True):
         """This method performs a GET request against the Freshservice API with multiple retries on failure.
 
@@ -51,6 +61,30 @@ class FreshPy(object):
         :raises: :py:exc:`freshpy.errors.exceptions.APIConnectionError`
         """
         return api.get_request_with_retries(self, uri, headers, return_json)
+
+    class Tickets(object):
+        """This class includes methods associated with Freshservice tickets."""
+        def __init__(self, freshpy_object):
+            """This method initializes the :py:class:`freshpy.core.freshpy.Tickets` inner class object.
+
+            .. versionadded:: 1.0.0
+
+            :param freshpy_object: The core :py:class:`freshpy.FreshPy` object
+            :type freshpy_object: class[freshpy.FreshPy]
+            """
+            self.freshpy_object = freshpy_object
+
+        def get_ticket(self, ticket_number):
+            """This function returns the data for a specific ticket.
+
+            .. versionadded:: 1.0.0
+
+            :param ticket_number: The ticket number for which to return data
+            :type ticket_number: str, int
+            :returns: JSON data for the given ticket
+            :raises: :py:exc:`freshpy.errors.exceptions.APIConnectionError`
+            """
+            return api.get_request_with_retries(self.freshpy_object, f'tickets/{ticket_number}')
 
     def __del__(self):
         """This method fully destroys the instance.
