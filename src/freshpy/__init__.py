055d64b007f719e6c8b4724f3529dd0ec3309a80
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-30 11:33:46 -0700

    Added to the __all__ value

--- src/freshpy/__init__.py
+++ src/freshpy/__init__.py
@@ -4,13 +4,13 @@
 :Synopsis:          This is the ``__init__`` module for the freshpy package
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     27 Dec 2021
+:Modified Date:     30 Dec 2021
 """
 
 from .core import FreshPy
 from .utils import version
 
-__all__ = ['core', 'FreshPy']
+__all__ = ['core', 'FreshPy', 'api', 'tickets']
 
 # Define the package version by pulling from the freshpy.utils.version module
 __version__ = version.get_full_version()
