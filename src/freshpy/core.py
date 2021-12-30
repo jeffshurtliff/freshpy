08d62f437ce36d93f8d91d9220188780a8a8ad72
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-12-30 13:12:08 -0700

    Updated get_ticket() to use the tickets module

--- src/freshpy/core.py
+++ src/freshpy/core.py
@@ -4,10 +4,11 @@
 :Synopsis:          Defines the core freshpy object used to interface with the Freshservice API
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     29 Dec 2021
+:Modified Date:     30 Dec 2021
 """
 
 from . import api
+from . import tickets as tickets_module
 from .utils import log_utils, version
 
 # Initialize logging
@@ -74,17 +75,19 @@ class FreshPy(object):
             """
             self.freshpy_object = freshpy_object
 
-        def get_ticket(self, ticket_number):
+        def get_ticket(self, ticket_number, include=None):
             """This function returns the data for a specific ticket.
 
             .. versionadded:: 1.0.0
 
             :param ticket_number: The ticket number for which to return data
             :type ticket_number: str, int
+            :param include: A string or iterable of `embedding <https://api.freshservice.com/#view_a_ticket>`_ options
+            :type include: str, tuple, list, set, None
             :returns: JSON data for the given ticket
             :raises: :py:exc:`freshpy.errors.exceptions.APIConnectionError`
             """
-            return api.get_request_with_retries(self.freshpy_object, f'tickets/{ticket_number}')
+            return tickets_module.get_ticket(self.freshpy_object, ticket_number, include)
 
     def __del__(self):
         """This method fully destroys the instance.
