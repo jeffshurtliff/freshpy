1e3126f9ed85330494a08a5442fc7f0464403a4d
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-01-02 22:07:28 -0700

    Updated the version to be v1.0.0.post0

--- src/freshpy/utils/version.py
+++ src/freshpy/utils/version.py
@@ -13,7 +13,7 @@ from . import log_utils
 logger = log_utils.initialize_logging(__name__)
 
 # Define special and global variables
-__version__ = "1.0.0"
+__version__ = "1.0.0.post1"
 
 
 def get_full_version():
