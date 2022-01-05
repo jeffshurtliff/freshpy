82f8aa8cbed4e2827bbc095854ac76434489cea1
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2022-01-05 09:15:54 -0700

    Changed version to 1.1.0 now that testing has been performed

--- src/freshpy/utils/version.py
+++ src/freshpy/utils/version.py
@@ -13,7 +13,7 @@ from . import log_utils
 logger = log_utils.initialize_logging(__name__)
 
 # Define special and global variables
-__version__ = "1.1.0b1"
+__version__ = "1.1.0"
 
 
 def get_full_version():
