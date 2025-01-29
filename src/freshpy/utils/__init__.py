85680aa6f216d281e69009990861a3eaa410ed66
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-01-29 13:51:35 -0700

    Added support for core_utils in utils/__init__.py file

--- src/freshpy/utils/__init__.py
+++ src/freshpy/utils/__init__.py
@@ -4,7 +4,7 @@
 :Synopsis:       This is the ``__init__`` module for the freshpy.utils modules
 :Created By:     Jeff Shurtliff
 :Last Modified:  Jeff Shurtliff
-:Modified Date:  27 Dec 2021
+:Modified Date:  29 Jan 2025
 """
 
-__all__ = ['log_utils', 'version']
+__all__ = ['core_utils', 'log_utils', 'version']
